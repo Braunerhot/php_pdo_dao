010ed0e0c602bd6903264437fa7251e18b611c66
Brauner Fernandes Azevedo <braunerhot@hotmail.com>
2021-02-12 03:00:14 -0300

    Metodos de Listar todos os registros

--- index.php
+++ index.php
@@ -2,10 +2,23 @@
 
 require_once("config.php");
 
-$root = new Usuario();
-
+//Carrega um usuario.
+/*$root = new Usuario();
 $root->loadbyId(4);
+echo $root;*/
 
-echo $root;
+// Carrega uma lista de usuarios.
+/*$lista = Usuario::getList();
+echo json_encode($lista);*/
+
+// Carrega uma lista de usuarios pelo login.
+/*$search = Usuario::search("ju");
+echo json_encode($search);*/
+
+// Carrega um usuario usando o login e a senha.
+$usuario = new Usuario();
+$usuario->login("root", "!@#12309");
+
+echo $usuario;
 
 ?>
